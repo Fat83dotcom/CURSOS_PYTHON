0eddc2e1ec1357d8f3d1cb053712786da5628cc8
fat83dotcom <fernando.quimic@gmail.com>
2022-09-09 00:10:40 -0300

    Comparação entre funçoes de ordenação

--- Python_Intermediario_PProcedural/funcoes_decoradoras.py
+++ Python_Intermediario_PProcedural/funcoes_decoradoras.py
@@ -5,9 +5,9 @@ from random import randint
 
 def salvaArquivo(valor, funcNome, resultado):
     with open('logExecutor.txt', 'a', encoding='utf-8') as arquivo:
-        arquivo.write(f'{datetime.today()} a função {funcNome.__name__} levou '
-                      f'{round(valor, 4)} segundos na execução')
-        arquivo.write(f'\nForam contados / ordenados: {resultado} números.\n\n')
+        arquivo.write(f'{datetime.today()}: A função {funcNome.__name__} levou '
+                      f'{round(valor, 4)} segundos na execução\n')
+        arquivo.write(f'{datetime.today()}: Foram contados / ordenados: {resultado} números.\n\n')
 
 
 def logTempoExecucao(funcao):
@@ -38,20 +38,37 @@ def contadorNumerico(numero) -> int:
 def insertionSort(listaNumeros):
     listaOriginal = listaNumeros.copy()
     listaDeTrabalho = listaNumeros.copy()
-    for i in range(len(listaDeTrabalho)):
-        for j in range(i)[::-1]:
-            if listaDeTrabalho[j + 1] < listaDeTrabalho[j]:
-                listaDeTrabalho[j], listaDeTrabalho[j + 1] = listaDeTrabalho[j + 1], listaDeTrabalho[j]
+    for numeroAnalizado in range(len(listaDeTrabalho)):
+        for numeroComparado in range(numeroAnalizado)[::-1]:
+            if listaDeTrabalho[numeroComparado + 1] < listaDeTrabalho[numeroComparado]:
+                listaDeTrabalho[numeroComparado], listaDeTrabalho[numeroComparado + 1] = \
+                    listaDeTrabalho[numeroComparado + 1], listaDeTrabalho[numeroComparado]
             else:
                 break
 
-    return f'números desordenados:{listaOriginal}, ordenados: {listaDeTrabalho},' \
-        f'{len(listaDeTrabalho)}'
+    return f'Quantidade de números ordenado: {len(listaDeTrabalho)}' \
+           f'\nNúmeros desordenados:{listaOriginal}\nOrdenados: {listaDeTrabalho},\n'
 
 
-contadorNumerico(100)
+@logTempoExecucao
+def insertionSort2(lista):
+    listaDeTrabalho = lista.copy()
+    listaOriginal = lista.copy()
+    for i in range(1, len(listaDeTrabalho)):
+        numeroAnalizado = listaDeTrabalho[i]
+        j = i - 1
+        while j >= 0 and numeroAnalizado < listaDeTrabalho[j]:
+            listaDeTrabalho[j + 1] = listaDeTrabalho[j]
+            j = j - 1
+        listaDeTrabalho[j + 1] = numeroAnalizado
+    return f'Quantidade de números ordenado: {len(listaDeTrabalho)}' \
+           f'\nNúmeros desordenados:{listaOriginal}\nOrdenados: {listaDeTrabalho},\n'
 
-embaralhados = embaralhadorNumerico(100)
-insertionSort(embaralhados)
 
-print(embaralhados)
+contadorNumerico(1000000)
+
+embaralhados = embaralhadorNumerico(1000000)
+# insertionSort(embaralhados)
+insertionSort2(embaralhados)
+
+# print(embaralhados)
